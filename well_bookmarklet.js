8cfa4f44518dab76f4a504b649dead6785537069
Zachariah Reitano <zreitano@gmail.com>
2016-08-30 13:04:20 -0400

    V5

--- well_bookmarklet.js
+++ well_bookmarklet.js
@@ -10,7 +10,7 @@ else {
 }
 
 function sendData() {
-   var url   = "http://localhost:3000/api_v1/articles.json";
+   var url   = "http://well.prehype.com/api_v1/articles.json";
    data = {}
    data.topic = {value: 1}
    data.url = location.href
