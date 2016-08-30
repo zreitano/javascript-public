821df68ea5a8ed1aea4862d3321d56fc2e94454d
Zachariah Reitano <zreitano@gmail.com>
2016-08-30 09:34:04 -0400

    v3

--- well_bookmarklet.js
+++ well_bookmarklet.js
@@ -1,4 +1,4 @@
-console.log("V2")
+console.log("V3")
 if (window.jQuery === undefined) {
     var script    = document.createElement( 'script' );
     script.src    = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
@@ -17,7 +17,7 @@ function sendData() {
       // data: $data,
       //send again in 5 minutes
       // success: function() { setTimeout(sendData, 5 * 60 * 1000); }
-      success: function() { alert("Article Submitted"); }
+      success: function() { alert("Article Submitted"); },
       error: function() { alert("WHOOPS");}
    });
 } 
