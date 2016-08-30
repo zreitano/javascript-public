f5aebcf7cbd2483fa0e330e9cb46703a82e9df64
Zachariah Reitano <zreitano@gmail.com>
2016-08-30 09:24:43 -0400

    bookmarklet v1

--- well_bookmarklet.js
+++ well_bookmarklet.js
@@ -1,23 +1,22 @@
-console.log("testing")
-// if (window.jQuery === undefined) {
-//     var script    = document.createElement( 'script' );
-//     script.src    = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-//     script.onload = function() { sendData; }
-//     document.body.appendChild(script);
-// }
-// else {
-//     sendData();
-// }
+if (window.jQuery === undefined) {
+    var script    = document.createElement( 'script' );
+    script.src    = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+    script.onload = function() { sendData; }
+    document.body.appendChild(script);
+}
+else {
+    sendData();
+}
 
-// function sendData() {
-//    var url   = "http://well.prehype.com/good-reads?url="+encodeURIComponent(location.href), 
-//    $.ajax({
-//       type: "GET",
-//       url: url,
-//       // data: $data,
-//       //send again in 5 minutes
-//       // success: function() { setTimeout(sendData, 5 * 60 * 1000); }
-//       success: function() { alert("Article Submitted"); }
-//       error: function() { alert("WHOOPS");}
-//    });
-// } 
+function sendData() {
+   var url   = "http://well.prehype.com/good-reads?url="+encodeURIComponent(location.href), 
+   $.ajax({
+      type: "GET",
+      url: url,
+      // data: $data,
+      //send again in 5 minutes
+      // success: function() { setTimeout(sendData, 5 * 60 * 1000); }
+      success: function() { alert("Article Submitted"); }
+      error: function() { alert("WHOOPS");}
+   });
+} 
